02da30041b24a34589bc4ce22c9bd7597c94d23e
hunjixin <1084400399@qq.com>
2021-04-06 17:20:11 +0800

    use messager token

--- api/messager-client.go
+++ api/messager-client.go
@@ -9,11 +9,10 @@ import (
 )
 
 func NewMessageRPC(messagerCfg *config.MessagerConfig) (client.IMessager, jsonrpc.ClientCloser, error) {
-	header := http.Header{}
+	headers := http.Header{}
 	if len(messagerCfg.Token) != 0 {
-		headers := http.Header{}
 		headers.Add("Authorization", "Bearer "+string(messagerCfg.Token))
 	}
 
-	return client.NewMessageRPC(context.Background(), messagerCfg.Url, header)
+	return client.NewMessageRPC(context.Background(), messagerCfg.Url, headers)
 }
