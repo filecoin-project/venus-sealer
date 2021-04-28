61ce5fbfcb904f3d8cbb63fd58269b812b471163
Mike <41407352+hunjixin@users.noreply.github.com>
2021-04-28 17:48:52 +0800

    use hash of params to speed up query (#34)

--- models/sqlite/workstate.go
+++ models/sqlite/workstate.go
@@ -1,6 +1,8 @@
 package sqlite
 
 import (
+	"crypto/sha256"
+	"encoding/hex"
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/venus-sealer/models/repo"
 	"github.com/filecoin-project/venus-sealer/types"
@@ -9,14 +11,14 @@ import (
 )
 
 type workerState struct {
-	Id     string `gorm:"column:id;type:varchar(36);primary_key;" json:"id"` // 主键
-	Method string `gorm:"uniqueIndex:method_params;column:method;type:varchar(256);" json:"method"`
-	Params string `gorm:"uniqueIndex:method_params;column:params;type:text;" json:"params"`
-
-	Status   string `gorm:"column:status;type:varchar(256);" json:"status"`
-	WorkId   string `gorm:"column:work_id;type:varchar(36);" json:"work_id"`
-	MinerID  uint64 `gorm:"column:miner_id;type:unsigned bigint;" json:"miner_id"`
-	SectorId uint64 `gorm:"column:sector_id;type:unsigned bigint;" json:"sector_id"`
+	Id         string `gorm:"column:id;type:varchar(36);primary_key;" json:"id"` // 主键
+	Method     string `gorm:"uniqueIndex:method_params_hash;column:method;type:varchar(256);" json:"method"`
+	ParamsHash string `gorm:"uniqueIndex:method_params_hash;column:params_hash;type:varchar(64);" json:"params_hash"`
+	Params     string `gorm:"column:params;type:text;" json:"params"`
+	Status     string `gorm:"column:status;type:varchar(256);" json:"status"`
+	WorkId     string `gorm:"column:work_id;type:varchar(36);" json:"work_id"`
+	MinerID    uint64 `gorm:"column:miner_id;type:unsigned bigint;" json:"miner_id"`
+	SectorId   uint64 `gorm:"column:sector_id;type:unsigned bigint;" json:"sector_id"`
 
 	WorkError string `gorm:"column:work_error;type:varchar(256);" json:"work_error"`
 
@@ -65,7 +67,7 @@ func newWorkerStateRepo(db *gorm.DB) *workerStateRepo {
 func (w *workerStateRepo) GetWorkerStateByWorkID(workId types.WorkID) (*types.WorkState, error) {
 	var workState workerState
 	err := w.DB.Table("worker_states").
-		First(&workState, "method=? AND params=?", workId.Method, workId.Params).Error
+		First(&workState, "method=? AND params_hash=?", workId.Method, w.hashParams(workId.Params)).Error
 	if err != nil {
 		return nil, err
 	}
@@ -75,7 +77,7 @@ func (w *workerStateRepo) GetWorkerStateByWorkID(workId types.WorkID) (*types.Wo
 func (w *workerStateRepo) HasState(workId types.WorkID) (bool, error) {
 	var count int64
 	err := w.DB.Table("worker_states").
-		Where("method=? AND params=?", workId.Method, workId.Params).Count(&count).Error
+		Where("method=? AND params_hash=?", workId.Method, w.hashParams(workId.Params)).Count(&count).Error
 	if err != nil {
 		return false, err
 	}
@@ -87,6 +89,7 @@ func (w *workerStateRepo) Save(workId types.WorkID, state *types.WorkState) erro
 		Id:             uuid.New().String(),
 		Method:         string(workId.Method),
 		Params:         workId.Params,
+		ParamsHash:     w.hashParams(workId.Params),
 		Status:         string(state.Status),
 		WorkId:         state.WorkerCall.ID.String(),
 		MinerID:        uint64(state.WorkerCall.Sector.Miner),
@@ -117,7 +120,7 @@ func (w *workerStateRepo) GetAllWorkState() ([]*types.WorkState, error) {
 }
 
 func (w *workerStateRepo) DeleteByWorkID(workId types.WorkID) error {
-	return w.DB.Delete(&workerState{}, "method=? AND params=?", workId.Method, workId.Params).Error
+	return w.DB.Delete(&workerState{}, "method=? AND params_hash=?", workId.Method, w.hashParams(workId.Params)).Error
 }
 
 func (w *workerStateRepo) UpdateStateByWorkID(state *types.WorkState, workId types.WorkID) error {
@@ -131,6 +134,11 @@ func (w *workerStateRepo) UpdateStateByWorkID(state *types.WorkState, workId typ
 		"miner_id":         state.WorkerCall.Sector.Miner,
 	}
 	return w.DB.Model(&workerState{}).
-		Where("method=? AND params=?", workId.Method, workId.Params).
+		Where("method=? AND params_hash=?", workId.Method, w.hashParams(workId.Params)).
 		UpdateColumns(updateClause).Error
+}
+
+func (w *workerStateRepo) hashParams(data string) string {
+	hash := sha256.Sum256([]byte(data))
+	return hex.EncodeToString(hash[:])
 }
