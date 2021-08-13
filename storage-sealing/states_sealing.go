1bc9c411fb4a8ff73fa58dad51fc77b182b4d104
Mike <41407352+hunjixin@users.noreply.github.com>
2021-08-13 17:36:40 +0800

    continue while network error (#90)

--- storage-sealing/states_sealing.go
+++ storage-sealing/states_sealing.go
@@ -3,6 +3,7 @@ package sealing
 import (
 	"bytes"
 	"context"
+	"strings"
 
 	"github.com/ipfs/go-cid"
 	"golang.org/x/xerrors"
@@ -426,12 +427,11 @@ func (m *Sealing) handlePreCommitWait(ctx statemachine.Context, sector types.Sec
 	log.Info("Sector precommitted: ", sector.SectorNumber)
 	mw, err := m.api.MessagerWaitMsg(ctx.Context(), sector.PreCommitMessage)
 	if err != nil {
-		return ctx.Send(SectorChainPreCommitFailed{err})
-		//if xerrors.Is(err, api.ErrFailMsg) {
-		//	return ctx.Send(SectorRemove{})
-		//} else {
-		//	return ctx.Send(SectorChainPreCommitFailed{err})
-		//}
+		if isUnRecoverError(err.Error()) {
+			return ctx.Send(SectorChainPreCommitFailed{err})
+		} else {
+			return ctx.Send(SectorRetryPreCommit{})
+		}
 	}
 
 	switch mw.Receipt.ExitCode {
@@ -703,12 +703,11 @@ func (m *Sealing) handleCommitWait(ctx statemachine.Context, sector types.Sector
 
 	mw, err := m.api.MessagerWaitMsg(ctx.Context(), sector.CommitMessage)
 	if err != nil {
-		return ctx.Send(SectorCommitFailed{xerrors.Errorf("failed to wait for porep inclusion: %w", err)})
-		//if xerrors.Is(err, api.ErrFailMsg) {
-		//	return ctx.Send(SectorRemove{})
-		//} else {
-		//	return ctx.Send(SectorCommitFailed{xerrors.Errorf("failed to wait for porep inclusion: %w", err)})
-		//}
+		if isUnRecoverError(err.Error()) {
+			return ctx.Send(SectorCommitFailed{xerrors.Errorf("failed to wait for porep inclusion: %w", err)})
+		} else {
+			return ctx.Send(SectorRetrySubmitCommit{})
+		}
 	}
 
 	switch mw.Receipt.ExitCode {
@@ -766,4 +765,10 @@ func (m *Sealing) handleProvingSector(ctx statemachine.Context, sector types.Sec
 	// TODO: Auto-extend if set
 
 	return nil
+}
+
+func isUnRecoverError(errString string) bool {
+	return !(strings.Contains(errString, "failed to submit proof for bulk verification (RetCode=32)") ||
+		strings.Contains(errString, "not enough funds") ||
+		strings.Contains(errString, "unlocked balance can not repay fee debt"))
 }
