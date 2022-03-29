ffd20cde571307fe18d0a6fb785f3d221af19b13
simlecode <hj1557986@gmail.com>
2022-03-29 23:37:34 +0800

    proving check --faulty, faults in storage list sectors

--- app/venus-sealer/proving.go
+++ app/venus-sealer/proving.go
@@ -450,6 +450,10 @@ var provingCheckProvableCmd = &cli.Command{
 			Name:  "storage-id",
 			Usage: "filter sectors by storage path (path id)",
 		},
+		&cli.BoolFlag{
+			Name:  "faulty",
+			Usage: "only check faulty sectors",
+		},
 	},
 	Action: func(cctx *cli.Context) error {
 		if cctx.Args().Len() != 1 {
@@ -510,6 +514,38 @@ var provingCheckProvableCmd = &cli.Command{
 			filter = map[abi.SectorID]struct{}{}
 			for _, decl := range decls {
 				filter[decl.SectorID] = struct{}{}
+			}
+		}
+
+		if cctx.Bool("faulty") {
+			parts, err := getAllPartitions(ctx, addr, nodeAPI)
+			if err != nil {
+				return xerrors.Errorf("getting partitions: %w", err)
+			}
+
+			if filter != nil {
+				for k := range filter {
+					set, err := parts.FaultySectors.IsSet(uint64(k.Number))
+					if err != nil {
+						return err
+					}
+					if !set {
+						delete(filter, k)
+					}
+				}
+			} else {
+				filter = map[abi.SectorID]struct{}{}
+
+				err = parts.FaultySectors.ForEach(func(s uint64) error {
+					filter[abi.SectorID{
+						Miner:  abi.ActorID(mid),
+						Number: abi.SectorNumber(s),
+					}] = struct{}{}
+					return nil
+				})
+				if err != nil {
+					return err
+				}
 			}
 		}
 
