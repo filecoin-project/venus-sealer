1a0ba18f25215caaadd85d30ace9a5c602efe2c9
simlecode <hj1557986@gmail.com>
2022-03-29 15:38:06 +0800

    Rename error message

--- storage-sealing/upgrade_queue.go
+++ storage-sealing/upgrade_queue.go
@@ -28,7 +28,7 @@ func (m *Sealing) MarkForSnapUpgrade(ctx context.Context, id abi.SectorNumber) e
 	}
 
 	if si.State != types.Proving {
-		return xerrors.Errorf("can't mark sectors not in the 'Proving' state for upgrade")
+		return xerrors.Errorf("unable to snap-up sectors not in the 'Proving' state")
 	}
 
 	if len(si.Pieces) != 1 {
